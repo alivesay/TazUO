b613d81850581facaa02b71ac9d8c6f5d9ecbe7b
TazmanianTad <bittiez@gmail.com>
2025-06-01 19:46:45 -0600

    Update build-all-platforms.sh and ClassicUO.Client.csproj

--- build-all-platforms.sh
+++ build-all-platforms.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
 CLIENT="src/ClassicUO.Client/"
-OUT="bin/publish/"
+OUT="bin/publish"
 
-dotnet publish "$CLIENT" -o "${OUT}win-x64" -c Release -r win-x64 /p:PublishSingleFile=true
-dotnet publish "$CLIENT" -o "${OUT}linux-x64" -c Release -r linux-x64 /p:PublishSingleFile=true
-dotnet publish "$CLIENT" -o "${OUT}osx-x64" -c Release -r osx-x64 /p:PublishSingleFile=true
-dotnet publish "$CLIENT" -o "${OUT}osx-arm64" -c Release -r osx-arm64 /p:PublishSingleFile=true
+# Array of runtime IDs
+RIDS=("win-x64" "linux-x64" "osx-x64" "osx-arm64")
+
+for RID in "${RIDS[@]}"; do
+  echo "Publishing for $RID..."
+  dotnet publish "$CLIENT" -o "${OUT}/${RID}" -c Release -r "$RID"
+
+  ZIPFILE="${OUT}/${RID}.zip"
+  echo "Zipping contents of ${OUT}/${RID} to $ZIPFILE ..."
+
+  [ -f "$ZIPFILE" ] && rm "$ZIPFILE"
+
+  (
+    cd "${OUT}/${RID}" || exit 1
+    zip -r -q "../${RID}.zip" ./*
+  )
+done
+
+echo "All done!"
